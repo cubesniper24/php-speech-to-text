7a8dec7871626863fe3c7e88bb7d554349db2994
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-03-16 17:11:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include '/GoogleSpeechToText.php';
 // Your API Key goes here.
 $apiKey = '';
 $speech = new GoogleSpeechToText($apiKey);
-$file = realpath('/quick.flac'); // Full path to the file.
+$file = realpath('./quick.flac'); // Full path to the file.
 $bitRate = 44100; // The bit rate of the file.
 $result = $speech->process($file, $bitRate, 'en-US');
 var_dump($result);
